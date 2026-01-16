6865477bd613d25d59ffe9672dfd94eafd66aa22
Apana Jude <androapana@gmail.com>
2026-01-16 01:12:03 +0000

    maintenance

--- app/Http/Ussd/States/ConfirmPackageState.php
+++ app/Http/Ussd/States/ConfirmPackageState.php
@@ -15,17 +15,40 @@ class ConfirmPackageState extends State
         $packageDuration = $this->record->get('selected_package_duration');
         $packageData = $this->record->get('selected_package_data');
         $packageUsers = $this->record->get('selected_package_users');
+        $packageUploadSpeed = $this->record->get('selected_package_upload_speed', 0);
+        $packageDownloadSpeed = $this->record->get('selected_package_download_speed', 0);
 
         // Format price with appropriate decimal places
         $priceDisplay = $packagePrice == floor($packagePrice) ?
             number_format($packagePrice, 0) :
             number_format($packagePrice, 2);
 
+        // Convert kbps to MB/s (divide by 1000 twice: kbps -> kB/s -> MB/s)
+        $uploadMBps = $packageUploadSpeed > 0 ? round($packageUploadSpeed / 1000, 1) : 0;
+        $downloadMBps = $packageDownloadSpeed > 0 ? round($packageDownloadSpeed / 1000, 1) : 0;
+
+        // Format speed display
+        $speedDisplay = '';
+        if ($downloadMBps > 0 && $uploadMBps > 0) {
+            if ($downloadMBps == $uploadMBps) {
+                $speedDisplay = $downloadMBps . 'MB/s';
+            } else {
+                $speedDisplay = $downloadMBps . '/' . $uploadMBps . 'MB/s';
+            }
+        }
+
         $this->menu
             ->line('Confirm Purchase:')
             ->line($packageName)
             ->line('GHS ' . $priceDisplay)
-            ->line('Data Limit: ' . $packageData)
+            ->line('Data Limit: ' . $packageData);
+
+        // Add speed line only if speed data is available
+        if (!empty($speedDisplay)) {
+            $this->menu->line('Speed: ' . $speedDisplay);
+        }
+
+        $this->menu
             ->line($packageUsers . ' User(s) Allowed')
             ->lineBreak()
             ->line('1) Pay')
